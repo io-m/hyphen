e32fd53b9f88694a3cffba53e23f3baac19058cd
Josip Miljak <miljak.josip@outlook.com>
2023-09-17 12:37:25 +0200

    Adding postgres connection.

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.21.0
 require (
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/google/uuid v1.3.1
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.13.0
 )
--- internal/shared/db/postgres_connection.go
+++ internal/shared/db/postgres_connection.go
@@ -1,1 +1,20 @@
 package db_connection
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/io-m/hyphen/pkg/constants"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresConnection() (*sqlx.DB, error) {
+	connStr := os.Getenv(constants.POSTGRES_CONNECTION)
+	// Instead of sql.Open, we use sqlx.Connect. This method combines sql.Open and db.Ping for us.
+	db, err := sqlx.Connect(constants.DRIVER, connStr)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to Postgres: %w", err)
+	}
+	return db, nil
+}
